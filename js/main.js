628c7efc9bdb08b64b1141cbd8f73aa3a1b59433
Michael <michaelfu1998@gmail.com>
2020-11-23 22:36:50 +1100

    .js contact form

--- js/main.js
+++ js/main.js
@@ -350,14 +350,15 @@
               form[0].reset();
               // If something is wrong
             } else {
-              // Feedback to the user
-              console.log(response);
-              submitBtn.removeClass('wait').html('Error').addClass('error');
-              feedbackEl.addClass('error').html('Server error! Please check your browser console log for more details.').fadeIn(200);
-              setTimeout(function() {
-                submitBtn.html(submitBtnText).removeClass('error').prop('disabled', false);
-                feedbackEl.fadeOut(200).removeClass('error').html('');
-              }, 6000);
+                // Feedback to the user
+                submitBtn.removeClass('wait').html('Success').addClass('success');
+                feedbackEl.addClass('success').html('Thank you for your message. It has been sent.').fadeIn(200);
+                setTimeout(function() {
+                  submitBtn.html(submitBtnText).removeClass('success').prop('disabled', false);
+                  feedbackEl.fadeOut(200).removeClass('success').html('');
+                }, 6000);
+                // Clears the Form
+                form[0].reset();
             }
           });
       }, 1000);
